7e6608e73aafb71a592b9f5dc5696632447cb9d4
imran <zunaeede@gmail.com>
2024-03-16 16:28:08 +0600

    main - done fetch tha data -  using asyncronise fetch method.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# React + Vite
+# React Country App.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+## Setp of making Process Country App.
+1. Basic Structure Setup
+2. fetch data.
+3. Display by using card.
+4. remove function add.
+5. Search country
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {useState, useEffect} from 'react';
 import './App.css'
 
+//REST country API url link
+const url = 'https://restcountries.com/v3.1/all';
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isloading, setIsloading] = useState(true);
+  const [error, setError] = useState(null);
+  const [countries, setCountries] = useState([]);
+
+
+// making function for fetch data using asyn method.
+  const fetchData = async (url) => {
+    setIsloading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setCountries(data);
+      setIsloading(false);
+      setError(null);
+      console.log(countries);
+      
+    } catch (error) {
+      setIsloading(false);
+      setError(error);
+    }
+  }
+
+
+  // Using Asyn method in Fetching Data(for that we need to make a function).
+  useEffect(() => {
+    fetchData(url)
+  }, [])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    Hello from App
     </>
   )
 }
