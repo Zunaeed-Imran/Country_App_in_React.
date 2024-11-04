3c0ff514cdf199e3d70cea685ab486b0f37e50d2
imran <zunaeede@gmail.com>
2024-11-04 12:03:33 +0600

    practice - setup the practice environment for the country app.

--- README.md
+++ README.md
@@ -1,21 +1,3 @@
-# React Country App.
+# React Country App Practice.
 
 
-## Setp of making Process Country App.
-1. Basic Structure Setup
-2. fetch data.
-3. Display by using card.
-4. remove function add.
-5. Search country
-
-## My Fault in this project.
-- i was not give () in the return what is why can't display falg's
-- 
-
-### Today Done Task.
-- Reading react documentation about the Fragment.
-- Learning country app part by part.
-- Practicing displaying the data form API.
-- Practicing remove country form Display API.
-- Learning Searching Data from API.
-- 
--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,15 @@
-import { useState, useEffect } from 'react';
 import './App.css';
 
-import Countries from './component/Countries';
-import Search from './component/Search';
 
-//REST country API url link
-const url = 'https://restcountries.com/v3.1/all';
+
 
 function App() {
-  const [isloading, setIsloading] = useState(true);
-  const [error, setError] = useState(null);
-  const [countries, setCountries] = useState([]);
-  const [filterCountries, setFilterCountries] = useState(countries);
-
-
-  // making function for fetch data using asyn method.
-  const fetchData = async (url) => {
-    setIsloading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setCountries(data);
-      setFilterCountries(data);
-      setIsloading(false);
-      setError(null);
-    } catch (error) {
-      setIsloading(false);
-      setError(error);
-    }
-  };
-
-
-
-  // Using Asyn method in Fetching Data(for that we need to make a function).
-  useEffect(() => {
-    fetchData(url);
-  }, []);
-
-
-  // making function for remove data.
-  const handleRemove = (name) => {
-    const filter = filterCountries.filter((countries) =>
-      countries.name.common !== name);
-    setFilterCountries(filter);
-  }
-
-  // making function search data.
-  const handleSearch = (searchValue) => {
-    let value = searchValue.toLowerCase();
-    const newCountry = countries.filter((country) => {
-      const countryName = country.name.common.toLowerCase();
-      return countryName.startsWith(value)
-    });
-    setFilterCountries(newCountry);
-  }
-
-
-  // we are using props in coutries 
   return (
-    <>
-      <h1>Country App</h1>
-
-      <Search onSearch={handleSearch} />
-
-      {isloading && <h2>Loding...</h2>}
-
-      {error && <h2>{error.message}</h2>}
-      
-      {countries && <Countries countries={filterCountries}
-        onRemoveCountry={handleRemove} />}
-    </>
-  );
+    <div>
+      Hello
+    </div>
+  )
 }
 
-export default App;
+export default App
+
