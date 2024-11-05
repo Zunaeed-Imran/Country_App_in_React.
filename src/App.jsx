33a65dad0176cd80fa440bdc939dbba49d4912f9
imran <zunaeede@gmail.com>
2024-11-05 09:56:14 +0600

    practice2 - setup the basic setup.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+
+function App() {
+
+  const flagName = 'https://restcountries.com/v3.1/all';
+
+  const [country, setCountry] = useState([]);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
